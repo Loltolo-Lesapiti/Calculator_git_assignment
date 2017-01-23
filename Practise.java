d1198372cc852057cbb40d7897606a367ea9628a
lesapiti-loltolo <petrolesapiti@gmail.com>
2017-01-23 20:47:15 +0000

    commiting from the branch

--- Practise.java
+++ Practise.java
@@ -1,5 +1,7 @@
 public class Practice{
 	public static void main (Strings args[]){
-		Syste.out.println("WE are trying out git on a lousy keyboard");
+		System.out.println("WE are trying out git on a lousy keyboard");
+		System.out.println("WE are trying out git on a lousy keyboard");
+System.out.println("WE are trying out git on a lousy keyboard");System.out.println("WE are trying out git on a lousy keyboard");System.out.println("WE are trying out git on a lousy keyboard");
 	}
 }
